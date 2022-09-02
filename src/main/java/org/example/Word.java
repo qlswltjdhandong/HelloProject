d6265c3d46565015280be5c5845d1fdf4c87c726
qlswltjdhandong <22100349@handong.ac.kr>
2022-09-02 10:16:07 +0900

    create HelloProject

---
+++ src/main/java/org/example/Word.java
@@ -0,0 +1,36 @@
+package org.example;
+
+public class Word {
+    private String name;
+    private String meaning;
+
+    public Word() {
+    }
+
+    public Word(String name, String meaning) {
+        this.name = name;
+        this.meaning = meaning;
+    }
+
+    public Word(String name) {
+        this.name = name;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+}
